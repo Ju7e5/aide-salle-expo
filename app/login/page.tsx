ee71b2f01bab4b75523ee0e1614fff0705a5c1b6
Jules Vallet <julesvallet@MacBook-Air-de-Jules.local>
2026-03-08 01:44:50 +0100

    fix: rename dynamic import to avoid name conflict with force-dynamic export

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,8 @@
-import dynamic from 'next/dynamic'
+import nextDynamic from 'next/dynamic'
 
 export const dynamic = 'force-dynamic'
 
-const LoginClient = dynamic(() => import('./LoginClient'), { ssr: false })
+const LoginClient = nextDynamic(() => import('./LoginClient'), { ssr: false })
 
 export default function LoginPage() {
   return <LoginClient />
